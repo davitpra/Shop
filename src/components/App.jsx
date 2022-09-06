80ad3cb97dade57218a6a5e8d10ab1741027a3ee
David <j.david.prado1716@gmail.com>
2022-09-06 14:01:48 -0500

    Estilos en los componentes de React

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../containers/Layout'
+import  Login  from '../containers/Login'
+
+const App = () => {
+    return (
+        <Layout>
+            <Login/>
+        </Layout>
+    )
+}
+
+export default App;
