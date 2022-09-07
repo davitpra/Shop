8419b591e7ddde234da5a35c8be6ab0ebaeb9ebf
David <j.david.prado1716@gmail.com>
2022-09-07 10:26:45 -0500

    Header en todas las rutas

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
+import Header from '../components/Header'
 
 function Home() {
     return (
-        <div>
-            <h1> React Home </h1>
-        </div>
+        <Header/>
     )
 }
 
