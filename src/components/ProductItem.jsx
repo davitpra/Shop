d12c6a7858aed8706bb5ce7563a28b1399b28d6e
David <j.david.prado1716@gmail.com>
2022-09-09 14:16:24 -0500

    Eliminando productos del carrito

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -12,13 +12,13 @@ const ProductItem = ({product}) => {
 
 	return (
 		<div className="ProductItem">
-			<img src= {product.images [0]} alt={product.title} />
+			<img src= {product.images} alt={product.title} />
 			<div className="product-info">
 				<div>
 					<p>${product.price}</p>
 					<p>{product.title}</p>
 				</div>
-				<figure onClick= {handleClick} >
+				<figure onClick= {()=> handleClick(product)} >
 					<img src={addToCartImage} alt="" />
 				</figure>
 			</div>
--- src/containers/MyOrder.jsx
+++ src/containers/MyOrder.jsx
@@ -1,11 +1,19 @@
 import React, {useContext} from 'react';
 import OrderItem from '@components/OrderItem';
 import AppContext from '../context/AppContext';
-import arrow from '../assets/icons/flechita.svg';
 import '@styles/MyOrder.scss';
+
+import arrow from '../assets/icons/flechita.svg';
 
 const MyOrder = () => {
 	const {state} = useContext (AppContext)
+
+	const sumTotal = () => {
+		const reducer = (acumulador, currentValue) =>
+			acumulador + currentValue.price;
+		const sum = state.cart.reduce(reducer, 0);
+		return sum;
+	};
 
 	return (
 		<aside className="MyOrder">
@@ -14,14 +22,14 @@ const MyOrder = () => {
 				<p className="title">My order</p>
 			</div>
 			<div className="my-order-content">
-				{state.cart.map (product => (
-					<OrderItem product = {product} key = {`OrderItem - ${product.id}`}/>
-				))}
+				{state.cart.map (product => 
+					<OrderItem product={ product } key = {`orderItem-${product.id}`}/>
+				)}
 				<div className="order">
 					<p>
 						<span>Total</span>
 					</p>
-					<p>$560.00</p>
+					<p>${sumTotal()}</p>
 				</div>
 				<button className="primary-button">
 					Checkout
